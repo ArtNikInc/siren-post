34b65a1aecfdd35d817517561e753137620c9e2b
nkozlov <nkozlov@vp.ru>
2022-08-26 18:00:28 +0600

    test-deploy

--- src/main/java/main/service/mapper/PostMapper.java
+++ src/main/java/main/service/mapper/PostMapper.java
@@ -2,7 +2,9 @@ package main.service.mapper;
 
 import main.dao.entity.Post;
 import main.dto.PostDto;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface PostMapper {
 
     PostDto toDto(Post post);
--- src/main/resources/db.changelog/changelogs/entity.sql
+++ src/main/resources/db.changelog/changelogs/entity.sql
@@ -1,14 +1,11 @@
 --liquibase formatted sql
 
 --changeset nkozlov:post
-CREATE SEQUENCE post_id_seq;
 CREATE TABLE post
 (
-    id            BIGINT        NOT NULL PRIMARY KEY ,
+    id            SERIAL        NOT NULL PRIMARY KEY ,
     title         varchar(250)  NOT NULL,
     text          text          NOT NULL
 );
 CREATE INDEX file_info_id_index ON post (id);
-ALTER SEQUENCE post_id_seq
-    OWNED BY post.id;
 --comment: Добавлена таблица post
